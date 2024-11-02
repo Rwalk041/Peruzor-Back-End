30a51bd8ee4b2cb05b41ab9ef98e5a2b6eda7927
WebDeveloper619 <webdevelopment619@gmail.com>
2024-11-03 02:04:26 +0500

    Changes

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,9 @@
 export default ({ env }) => ({
   auth: {
-    secret: env("ADMIN_JWT_SECRET"),
+    secret: env(
+      "ADMIN_JWT_SECRET",
+      "c709ab18f3054b1696689e8239d0dc929a02efd29651803ea09b2f6037d9185f"
+    ),
   },
   apiToken: {
     salt: env(
@@ -8,6 +11,7 @@ export default ({ env }) => ({
       "bcf52718753536a1feaca004089def966c89e4c997deae11129012cf02f9802f"
     ),
   },
+
   transfer: {
     token: {
       salt: env("TRANSFER_TOKEN_SALT"),
