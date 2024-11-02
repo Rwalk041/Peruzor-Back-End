1a9878aba0a132c17db133dc7d75e914d6d5df1a
WebDeveloper619 <webdevelopment619@gmail.com>
2024-11-03 01:55:26 +0500

    chanes

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,20 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env(
+      "API_TOKEN_SALT",
+      "bcf52718753536a1feaca004089def966c89e4c997deae11129012cf02f9802f"
+    ),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
