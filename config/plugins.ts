0de5df5b326eba563570124c3d7a0d162eb9efc4
WebDeveloper619 <webdevelopment619@gmail.com>
2024-11-06 05:03:23 +0500

    jwt

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
 export default () => ({});
+module.exports = {
+  // Other plugins config
+  "users-permissions": {
+    config: {
+      jwtSecret: "qUX/lfZf9ed59GIEc399WQ==", //process.env.JWT_SECRET ||
+    },
+  },
+};
